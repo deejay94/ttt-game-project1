8921e05a2feed6a78a1ccdff9576568b6a58423f
deejay94 <dahjoyner12@gmail.com>
2017-08-31 11:28:40 -0400

    Changes for project 1
    The click event now displays 'X'.

---
+++ assets/scripts/books/gamelogic.js
@@ -0,0 +1,35 @@
+const tttArrray = [[null, null, null], [null, null, null], [null, null, null]]
+
+// if a player clicks on the index and the value is null, display x
+
+// $(() => {
+//   $('button').on('click', function () {
+//     event.preventDefault()
+//     console.log('button works')
+//     $(this).text('X')
+//   })
+// })
+
+const name = function (event) {
+  event.preventDefault()
+  console.log('button works')
+  $(this).text('X')
+}
+
+// const displayX = function (event) {
+//   event.preventDefault()
+//   console.log('heard a click')
+//   $(this).text('x')
+// }
+//
+// const name = function () {
+//   $('.col-xs-4').on('click', displayX())
+//   console.log('heard a click')
+// }
+// $("col-xs-4 box draw-bottom draw-right").on("click", )
+// if the index value is 'x', display "choose another square".
+// if
+
+module.exports = {
+  name
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,8 +7,26 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
+const name = function (event) {
+  event.preventDefault()
+  console.log('button works')
+  $(this).text('X')
+}
+
+$(() => {
+  $('button').on('click', function (event) {
+    event.preventDefault()
+    console.log('button works')
+    $(this).text('X')
+  })
+})
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+module.eports = {
+  name
+}
