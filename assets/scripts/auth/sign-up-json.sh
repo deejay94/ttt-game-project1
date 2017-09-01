38f13f3f5c089d39bd1f91397a9cbd783d374c3d
deejay94 <dahjoyner12@gmail.com>
2017-09-01 13:37:02 -0400

    made changes to game logic

---
+++ assets/scripts/auth/sign-up-json.sh
@@ -0,0 +1,21 @@
+
+
+
+# API="${API_ORIGIN:-http://httpbin.org}"
+# URL_PATH="/post"
+API="${API_ORIGIN:-https://aqueous-atoll-85096.herokuapp.com/}"
+URL_PATH="/sign-up"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'"
+    }
+  }'
+
+echo
