e89a0860d017fe7d984c68448dc26ff637d29926
deejay94 <dahjoyner12@gmail.com>
2017-09-01 13:35:56 -0400

    Changes for project 1
    I made changes to game logic

--- assets/scripts/books/gamelogic.js
+++ assets/scripts/books/gamelogic.js
@@ -1,37 +1,3 @@
-const tttArrray = [[null, null, null], [null, null, null], [null, null, null]]
+const tttArrray = [null, null, null, null, null, null, null, null, null]
 
-// if a player clicks on the index and the value is null, display x
-
-// $(() => {
-//   $('button').on('click', function () {
-//     event.preventDefault()
-//     console.log('button works')
-//     $(this).text('X')
-//   })
-// })
-
-if ()
-
-const name = function (event) {
-  event.preventDefault()
-  console.log('button works')
-  $(this).text('X')
-}
-
-// const displayX = function (event) {
-//   event.preventDefault()
-//   console.log('heard a click')
-//   $(this).text('x')
-// }
-//
-// const name = function () {
-//   $('.col-xs-4').on('click', displayX())
-//   console.log('heard a click')
-// }
-// $("col-xs-4 box draw-bottom draw-right").on("click", )
-// if the index value is 'x', display "choose another square".
-// if
-
-module.exports = {
-  name
-}
+let clickCounter = 0;
