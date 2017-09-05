82224d1376a434f0d2c86c50ff92a0bc480b8eac
deejay94 <dahjoyner12@gmail.com>
2017-09-05 09:39:20 -0400

    Game doesn't start until someone signs in
    The game won't start until someone is signed in. The board will not
    even appear until signin is submitted.

--- assets/scripts/books/gamelogic.js
+++ assets/scripts/books/gamelogic.js
@@ -2,6 +2,13 @@
 const tttArrray = ['', '', '', '', '', '', '', '', '']
 let clickCounter = 0
 
+ $(function () {
+  $('.box').hide()
+  $('#sign-in').on('submit', function() {
+  $('.box').toggle()
+})
+})
+
 const player = function () {
   if (clickCounter % 2 === 1) {
     return 'X'
@@ -34,7 +41,7 @@ const displayLetter = function (event) {
   }
 }
 
-const newGame = function (event) {
+const resetGame = function (event) {
   event.preventDefault()
   $('.box').text('')
   $('#message').text('')
@@ -47,35 +54,35 @@ const whoWon = function () {
     if (($('#box1').text() === $('#box2').text() && $('#box2').text() === $('#box3').text()) && ($('#box1').text() === 'X' || $('#box1').text() === 'O')) {
       $('#message').text(player() + ' is the Winner!!')
       $('.box').off()
-      newGame()
+      resetGame()
     } else if (($('#box1').text() === $('#box5').text() && $('#box5').text() === $('#box9').text()) && ($('#box1').text() === 'X' || $('#box1').text() === 'O')) {
       $('#message').text(player() + ' is the Winner!!')
       $('.box').off()
-      newGame()
+      resetGame()
     } else if (($('#box1').text() === $('#box4').text() && $('#box4').text() === $('#box7').text()) && ($('#box1').text() === 'X' || $('#box1').text() === 'O')) {
       $('#message').text(player() + ' is the Winner!!')
       $('.box').off()
-      newGame()
+      resetGame()
     } else if (($('#box2').text() === $('#box5').text() && $('#box5').text() === $('#box8').text()) && ($('#box2').text() === 'X' || $('#box2').text() === 'O')) {
       $('#message').text(player() + ' is the Winner!!')
       $('.box').off()
-      newGame()
+      resetGame()
     } else if (($('#box3').text() === $('#box6').text() && $('#box6').text() === $('#box9').text()) && ($('#box3').text() === 'X' || $('#box3').text() === 'O')) {
       $('#message').text(player() + ' is the Winner!!')
       $('.box').off()
-      newGame()
+      resetGame()
     } else if (($('#box4').text() === $('#box5').text() && $('#box5').text() === $('#box6').text()) && ($('#box4').text() === 'X' || $('#box4').text() === 'O')) {
       $('#message').text(player() + ' is the Winner!!')
       $('.box').off()
-      newGame()
+      resetGame()
     } else if (($('#box3').text() === $('#box5').text() && $('#box5').text() === $('#box7').text()) && ($('#box3').text() === 'X' || $('#box3').text() === 'O')) {
       $('#message').text(player() + ' is the Winner!!')
       $('.box').off()
-      newGame()
+      resetGame()
     } else if (($('#box7').text() === $('#box8').text() && $('#box8').text() === $('#box9').text()) && ($('#box7').text() === 'X' || $('#box7').text() === 'O')) {
       $('#message').text(player() + ' is the Winner!!')
       $('.box').off()
-      newGame()
+      resetGame()
     }
   } else {
     $('#message').text('Issa Draw')
@@ -85,5 +92,5 @@ const whoWon = function () {
 module.exports = {
   displayLetter,
   whoWon,
-  newGame
+  resetGame
 }
