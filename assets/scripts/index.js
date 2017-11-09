6d8e1e4071c4f86c73638f5dfedaa2722b8070b0
deejay94 <dahjoyner12@gmail.com>
2017-11-08 22:18:49 -0500

    Add modal to work when document loads
    Add modal to work when document loads

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,20 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./books/events')
+const modal = document.getElementById('myModal')
+const btn = document.getElementById('myBtn')
+const span = document.getElementsByClassName('close')[0]
+btn.onclick = function () {
+  modal.style.display = 'block'
+}
+span.onclick = function () {
+  modal.style.display = 'none'
+}
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none'
+  }
+}
 
 $(() => {
   setAPIOrigin(location, config)
